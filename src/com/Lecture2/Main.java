4aeec4ce31f3bfb622b3a70ecbcb6b7c56249b50
VladislavStaryh <vlad.star.08@gmail.com>
2017-08-30 21:04:39 +0300

    Class Person implemented.

---
+++ src/com/Lecture2/Main.java
@@ -0,0 +1,23 @@
+package com.Lecture2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person Jack = new Person(true, "Jack");
+        Person Mary = new Person(false, "Mary");
+        Person John = new Person(true, "John");
+        Person Amy = new Person(false, "Amy");
+
+        System.out.println(Jack.marry(Mary));
+        System.out.println(John.marry(Amy));
+        System.out.println(Jack.marry(John));
+        System.out.println(Mary.marry(Amy));
+        System.out.println(Jack.marry(Mary));
+        System.out.println(John.marry(Amy));
+        System.out.println(John.marry(Mary));
+        System.out.println(Jack.marry(Amy));
+
+    }
+
+
+}
---
+++ src/com/Lecture2/Person.java
@@ -0,0 +1,41 @@
+package com.Lecture2;
+
+public class Person {
+    private final boolean man;
+    private final String name;
+    private Person spouse;
+
+    public Person(boolean man, String name) {
+        this.man = man;
+        this.name = name;
+    }
+
+    public boolean marry(Person person) {
+        if (person.man == this.man || person.spouse == this)
+            return false;
+
+        if (this.spouse != null) {
+            this.spouse.divorce();
+            this.divorce();
+
+        }
+
+        if (person.spouse != null) {
+            person.spouse.divorce();
+            person.divorce();
+        }
+
+        this.spouse = person;
+        person.spouse = this;
+
+        return true;
+    }
+
+    public boolean divorce() {
+        if (spouse != null) {
+            spouse = null;
+            return true;
+        }
+        return false;
+    }
+}
